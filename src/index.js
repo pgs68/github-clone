93baa587b51df520706a0222646336ec0e42e6e9
Paula Guadalajara <pgs68@alu.ua.es>
2022-04-22 20:15:32 +0200

    Project configuration

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./components/Home";
+
+ReactDOM.render(
+    <div>
+        <Home />
+    </div>, 
+    document.getElementById("app")
+);
